2a2002a2a966bf3f7a4eaec2da93f0e10163eac7
Dominique Paul <dominique.c.a.paul@gmail.com>
2024-01-17 21:51:37 -0600

    Fix static deploy; tweak landing page text and order

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -6,15 +6,15 @@ export default function Home() {
     <>
       {/* <MaxWidthWrapper className='mb-12 mt-12 sm:mt-40 flex flex-col items-center justify-center text-center'> */}
       <MaxWidthWrapper className='mx-auto mb-12 pt-16 sm:mt-40 flex flex-col items-center justify-center text-center'>
-          <h1 className="max-w-4xl text-5xl - font-bold md:text-6xl lg:text-7xl">
-            Record a voice memo. Send it as a <span className="text-orange-600">letter</span>.
-          </h1>
-          <p className="mt-5 max-w-prose text-zinc-700 sm:text-lg">Effortlessly create and send physical letters from your telegram and whatsapp chat - simply record, edit, and mail, all without leaving the chat.</p>
           <div className='mx-auto mt-4 mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50'>
             <p className='text-sm font-semibold text-gray-700'>
               GrannyMail is currently in closed alpha.
             </p>
           </div>
+          <h1 className="max-w-4xl text-5xl - font-bold md:text-6xl lg:text-7xl">
+            Record a voice memo. Send it as a <span className="text-orange-600">letter</span>.
+          </h1>
+          <p className="mt-5 max-w-prose text-zinc-700 sm:text-lg">Effortlessly create and send physical letters from your telegram and whatsapp chat - simply record, edit, and mail, all without leaving the chat.</p>
           {/* <Link href="/dashboard" target="_blank" className={buttonVariants({ size: "lg", className: "mt-5", variant: "outline" })}>Join Waitlist <ArrowRight className="ml-2 h-4 w-5" /></Link> */}
       </MaxWidthWrapper>
 
