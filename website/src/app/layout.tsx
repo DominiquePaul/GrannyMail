5febc963e47b26daaa5654f8a96f7b307b5f875b
Dominique Paul <dominique.c.a.paul@gmail.com>
2024-01-17 18:07:18 -0600

    using relative paths now

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -1,4 +1,4 @@
-import { cn } from '@/lib/utils'
+import { cn } from '../lib/utils'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -1,7 +1,4 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper"
-import Link from "next/link"
-import { ArrowRight } from "lucide-react"
-import { buttonVariants } from "@/components/ui/button"
 
 
 export default function Home() {
