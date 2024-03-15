e37f6257ed74bcedcfa41d1ee2b31d0fdd6fc7be
Dominique Paul <dominique.c.a.paul@gmail.com>
2024-03-15 18:55:07 -0300

    Add debug statement to handle_event for stripe

--- chatbot/grannymail/integrations/stripe_payments.py
+++ chatbot/grannymail/integrations/stripe_payments.py
@@ -62,7 +62,7 @@ def handle_event(
     # fetch necessary information to handle transaction
     checkout_info = stripe_event["data"]["object"]
     client_reference_id = checkout_info.get("client_reference_id")
-    logger.debug("Checkout Info: \n\n" + str(checkout_info))
+    logger.info("Checkout Info: \n\n" + str(checkout_info))
     payment_link_id = checkout_info["payment_link_id"]
     order = uow.orders.get_one(client_reference_id)
     credits_bought = _get_credits_bought(payment_link_id)
